d97e48b67e43909696fe59c0743685b3f38df4c6
9bany <bany.y0599@gmail.com>
2023-08-09 17:14:20 +0700

    move server command under the root command

--- cmd/cammands.go
+++ cmd/cammands.go
@@ -1,48 +1,21 @@
 package cmd
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/9bany/task/api"
-	db "github.com/9bany/task/db/sqlc"
-	httpclient "github.com/9bany/task/http_client"
-	"github.com/9bany/task/util"
 
 	_ "github.com/lib/pq"
 
 	"github.com/spf13/cobra"
 )
 
-func startServer(cmd *cobra.Command, args []string) {
-	var err error
-	config, err := util.LoadConfig("./")
-	if err != nil {
-		log.Fatalln("Can not load config file", err)
-	}
-	conn, err := sql.Open(config.DBDriver, config.DBSource)
-	if err != nil {
-		log.Fatal("Can not connect to database:", err)
-	}
-
-	store := db.New(conn)
-
-	iframelyClient := httpclient.New(config.IframeURL)
-
-	server, err := api.NewServer(config, store, iframelyClient)
-	if err != nil {
-		log.Fatalln("Can not start server", err)
-	}
-	server.Start(config.ServerAddress)
-}
-
 var rootCmd = &cobra.Command{
-	Use:   "serve",
+	Use:   "task",
 	Short: "start server",
 	Long:  ``,
-	Run:   startServer,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("task --help")
+	},
 }
 
 func Execute() {
