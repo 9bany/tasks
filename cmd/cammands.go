33b7eabc56d0e096490912a8927a671661de3d74
9bany <bany.y0599@gmail.com>
2023-08-09 17:10:26 +0700

    public-get-site-api

---
+++ cmd/cammands.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/9bany/task/api"
+	db "github.com/9bany/task/db/sqlc"
+	httpclient "github.com/9bany/task/http_client"
+	"github.com/9bany/task/util"
+
+	_ "github.com/lib/pq"
+
+	"github.com/spf13/cobra"
+)
+
+func startServer(cmd *cobra.Command, args []string) {
+	var err error
+	config, err := util.LoadConfig("./")
+	if err != nil {
+		log.Fatalln("Can not load config file", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Can not connect to database:", err)
+	}
+
+	store := db.New(conn)
+
+	iframelyClient := httpclient.New(config.IframeURL)
+
+	server, err := api.NewServer(config, store, iframelyClient)
+	if err != nil {
+		log.Fatalln("Can not start server", err)
+	}
+	server.Start(config.ServerAddress)
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "start server",
+	Long:  ``,
+	Run:   startServer,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
